e3ccd7598decd477d54c1b629699b73ada46d4be
Jose Luis Poveda <jlpoveda@stackscale.com>
2018-09-17 10:30:49 +0200

    Changed setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email='opensource@netapp.com',
     license='Apache 2.0',
     url='TBD',
-    version='1.0.4',
+    version='2018.10.30',
 
     packages=find_packages(),
     install_requires=[
